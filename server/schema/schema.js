8a109d979cfa2e77b763cc41b64791bd1cc90ccf
Jakob Persons <jakobep24@gmail.com>
2022-01-29 17:51:45 -0500

    create schema

---
+++ server/schema/schema.js
@@ -0,0 +1,20 @@
+const graphql = require('graphql');
+
+
+// define schema to describe data in graphql graph
+
+
+// 1. define object types
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+// route queries
