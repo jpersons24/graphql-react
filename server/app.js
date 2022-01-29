4714663c3553090c65e73b8fa9fefdafc6afbece
Jakob Persons <jakobep24@gmail.com>
2022-01-29 12:44:59 -0500

    express app setup

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+// express listen to port 4000
 app.listen(4000, () => {
+  // test server is running by logging to console
   console.log('now listening for requests on port 4000');
 });
