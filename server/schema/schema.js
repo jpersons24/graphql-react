d515e64cd5c695170b46171c54d326cfd523c8b1
Jakob Persons <jakobep24@gmail.com>
2022-01-30 09:57:26 -0500

    add author type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,4 @@
 const graphql = require('graphql');
-const { buildResolveInfo } = require('graphql/execution/execute');
 const _ = require('lodash');
 
 
@@ -9,7 +8,8 @@ const {
   GraphQLObjectType, 
   GraphQLString, 
   GraphQLSchema,
-  GraphQLID
+  GraphQLID,
+  GraphQLInt,
 } = graphql;
 
 // dummy data
@@ -19,6 +19,12 @@ let books = [
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
 ]
 
+var authors = [
+	{ name: "Patrick Rothfuss", age: 44, id: '1' },
+	{ name: "Brandon Sanderson", age: 42, id: '2' },
+	{ name: "Terry Pratchett", age: 66, id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
 	name: "Book",
 	fields: () => ({
@@ -27,6 +33,15 @@ const BookType = new GraphQLObjectType({
 		genre: { type: GraphQLString },
 	}),
 });
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+})
 
 // route queries are initial entry points into Graph
 const RootQuery = new GraphQLObjectType({
@@ -38,11 +53,18 @@ const RootQuery = new GraphQLObjectType({
 				id: { type: GraphQLID },
 			},
 			resolve(parent, args) {
-				// code to get data from db / other source
-        // args.id is converted to string within this function even though it's type ID
-
-				// use lodash to .find() a book with certain id from books array
+        // code to get data from db / other soruce
 				return _.find(books, { id: args.id });
+			},
+		},
+		author: {
+			type: AuthorType,
+			args: {
+				id: { type: GraphQLID },
+			},
+			resolve(parent, args) {
+				// code to get data from db / other source
+				return _.find(authors, { id: args.id });
 			},
 		},
 	},
