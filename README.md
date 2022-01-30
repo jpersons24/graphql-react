5e4e219c05e16d38dfcc33b0ff61e6ec2aff035f
Jakob Persons <jakobep24@gmail.com>
2022-01-30 11:48:16 -0500

    update root query, readme

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 # Initial Setup
 
-## Project Structure
-- server
-  - app.js
-  - package.json
-  - package-lock.json
-  - node_modules
-- README.md
+## Technologies Used
+- Server
+  - Node.js
+    - Express
+    - GraphQL server
+- Database
+  - Mongo DB
+  - using mLab to emulate Mongo DB
+- Client (Browser)
+  - React
+  - Apollo
+
+_Graphiql tool will be used to test GraphQL queries_
+
+## Initial Setup
+
+Create directory that will be initialized as git repository AND contain you're entire project.
+
+Within the root of that directory, run `npm init` to initialize Node project. (This will create package.json file).
+You can then create the server directory as a direct child of your application directory.
+
+You can then start installing the needed dependencies and setting up the basic skeleton of the server.
+
+> ```
+>   npm install express
+>   npm install graphql
+>   npm install express-graphql
+> ```
+
+`express-graphql`_expects v 15.8.0_, you may get an error and need to change the graphql version being used if necessary.
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,13 +10,17 @@ const {
   GraphQLSchema,
   GraphQLID,
   GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // dummy data
 let books = [
-  { name: 'Name of the Wind', genre: 'Fatnasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+  { name: 'Name of the Wind', genre: 'Fatnasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Color of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
 ]
 
 var authors = [
@@ -31,17 +35,29 @@ const BookType = new GraphQLObjectType({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId })
+      }
+    }
 	}),
 });
 
 const AuthorType = new GraphQLObjectType({
-  name: "Author",
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    age: { type: GraphQLInt }
-  })
-})
+	name: "Author",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
+		books: {
+			type: new GraphQLList(BookType),
+      resolve(parent, args){
+        return _.filter(books, { authorId: parent.id })
+      }
+		},
+	}),
+});
 
 // route queries are initial entry points into Graph
 const RootQuery = new GraphQLObjectType({
@@ -67,6 +83,20 @@ const RootQuery = new GraphQLObjectType({
 				return _.find(authors, { id: args.id });
 			},
 		},
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args){
+        // return entire list of books
+        return books
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args){
+        // return entire list of authors
+        return authors
+      }
+    }
 	},
 });
 
